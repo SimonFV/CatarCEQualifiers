c2a701d4185131d59cd4a093a515fe2a0e34908a
Simón Fallas <sfv0215@gmail.com>
2021-11-02 16:03:56 -0600

    Co-authored-by: JoseJ1806 <JoseJ1806@users.noreply.github.com>

--- CatarCEQualifiers.rkt
+++ CatarCEQualifiers.rkt
@@ -3,6 +3,26 @@
 ; Paquetes necesarios
 (require racket/gui)
 (require dyoo-while-loop)
+(require (file "genetics.rkt"))
+
+
+; Constantes
+(define MIN_X 0)
+(define MAX_X 1200)
+(define MIN_Y 100)
+(define MAX_Y 600)
+(define CENTER_X 600)
+(define CENTER_Y 350)
+(define LEFT_LIMIT 200)
+(define RIGHT_LIMIT 800)
+(define LEFT_KEEPER 80)
+(define RIGHT_KEEPER 1120)
+
+(define def_limits (list MIN_X LEFT_LIMIT MIN_Y MAX_Y))
+
+; Flags
+
+
 
 ; Colores
 (define no-pen (new pen% [style 'transparent]))
@@ -15,13 +35,40 @@
 (define white-brush (new brush% [color "white"]))
 
 
-; OBJETOS
+; ELEMENTOS
 
-; Jugador
-(define team1 '((50 50) (200 300) (150 150) (100 300)))
+; Equipos. Estructura '((posX posY velocidad fuerza habilidad)...(...))
+(define team_blue_keep (first_gen 1))
+(define team_blue_def (first_gen 4))
+(define team_blue_mid (first_gen 4))
+(define team_blue_atck (first_gen 2))
+
+(define team_red_keep (first_gen 4))
+(define team_red_def (first_gen 4))
+(define team_red_mid (first_gen 4))
+(define team_red_atck (first_gen 2))
+
+; Lista temporal que alberga los jugadores de las nuevas generaciones
+(define new_players '())
+; Azul
+(define selected_players_blue_keep '())
+(define selected_players_blue_def '())
+(define selected_players_blue_mid '())
+(define selected_players_blue_atck '())
+; Rojo
+(define selected_players_red_keep '())
+(define selected_players_red_def '())
+(define selected_players_red_mid '())
+(define selected_players_red_atck '())
+
 
 ; Balon
-(define ball '((178 103) (178 103) (178 103) (178 103)))
+(define ball (list CENTER_X CENTER_Y))
+(define ball_speed '(0 0))
+
+; Marcador
+(define goals '(0 0))
+
 
 
 ; INTERFAZ
@@ -35,6 +82,7 @@
 ;Contenedor del marcador
 (define scoreboard(new horizontal-pane%[parent container]))
 
+#|
 ;Marcador del equipo azul
 (define blue_score(new message% [parent scoreboard]
                        [label "0"]
@@ -43,9 +91,9 @@
 
 ;Marcador del equipo rojo
 (define red_score(new message% [parent scoreboard]
-                       [label "0"]
-                       [font(make-font #:size 32 #:family 'roman #:weight 'bold)]
-                       [horiz-margin 289]))
+                      [label "0"]
+                      [font(make-font #:size 32 #:family 'roman #:weight 'bold)]
+                      [horiz-margin 289]))|#
 
 ; Canvas donde se dibujan todos los elementos
 (define game_field (new canvas% [parent frame]
@@ -65,19 +113,37 @@
                            (send dc set-brush black-brush)
                            (send dc draw-rectangle 0 0 1200 100)
                            (send dc set-font (make-font #:size 32 #:family 'roman #:weight 'bold))
-                           (send dc set-text-foreground "blue")                         
-                           (send dc draw-text "Blue Team" 200 25)
-                           (send dc set-text-foreground "red")                         
-                           (send dc draw-text "Red Team" 800 25)
+                           (send dc set-text-foreground "blue")
+                           (send dc draw-text (~a "Blue Team: " (car goals)) 200 25)
+                           (send dc set-text-foreground "red")
+                           (send dc draw-text (~a "Read Team: " (cadr goals)) 800 25)
 
                            ; Dibuja la bola
                            (send dc set-brush white-brush)
-                           (send dc draw-ellipse (caar ball) (cadar ball) 10 10)
+                           (send dc draw-ellipse (car ball) (cadr ball) 30 30)
 
                            ; Dibuja los jugadores
+                           ; Azul
                            (send dc set-brush blue-brush)
-                           (for ([player team1])
-                             (send dc draw-ellipse (car player) (cadr player) 10 10))
+                           (for ([player team_blue_keep])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
+                           (for ([player team_blue_def])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
+                           (for ([player team_blue_mid])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
+                           (for ([player team_blue_atck])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
+
+                           ; Rojo
+                           (send dc set-brush red-brush)
+                           (for ([player team_red_keep])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
+                           (for ([player team_red_def])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
+                           (for ([player team_red_mid])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
+                           (for ([player team_red_atck])
+                             (send dc draw-ellipse (car player) (cadr player) 30 30))
 
                            )]))
 
@@ -88,19 +154,161 @@
 ; OPERACIONES DE CONTROL
 
 ; Calcula hacia donde debe moverse el jugador para llegar a su destino
-(define (route current end)
-  (cond [(< current end) (+ 1 current)]
-        [(> current end) (- current 1)]
-        [else current]))
+(define (route current end speed)
+  (cond [(< current (- end speed)) (+ (* 1 speed) current)]
+        [(> current (+ end speed)) (- current (* 1 speed))]
+        [else end]))
 
+; Actualiza la lista de los jugadores para que se dirijan hacia el destino
 (define (update_pos old_player new_player)
-  (list (route (car old_player) (car new_player)) (route (cadr old_player) (cadr new_player))))
+  (list (route (car old_player) (car new_player) (caddr new_player))
+        (route (cadr old_player) (cadr new_player) (caddr new_player))
+        (caddr new_player) (cadddr new_player) (cadddr (cdr new_player))))
+
+
+
+
+; Cambia la velocidad inicial del balon que empiece a animarse como si se pateara
+(define (kick player direction)
+  (set! ball_speed [list
+                    (* (cadddr player) direction 3)
+                    (round (/ (random -20 20) (+ 1 (cadddr (cdr player))) ))]))
+
+; Revisa si hay algun jugador tocando con el balon y lo patea
+(define (close_enough player direction)
+  (cond [(and (< (abs (- (car player) (car ball))) 30) (< (abs (- (cadr player) (cadr ball))) 30))
+         (kick player direction)]))
+
+; Cambia la posicion del balon en base a su velocidad actual
+(define (kicked)
+  (begin
+    (set! ball (list (+ (car ball) (car ball_speed)) (+ (cadr ball) (cadr ball_speed))) )
+    (cond [(not (= 0 (car ball_speed)))
+           (set! ball_speed (list
+                             (round (- (car ball_speed) (/ (car ball_speed) (abs (car ball_speed))) ))
+                             (cadr ball_speed)))])
+    (cond [(not (= 0 (cadr ball_speed)))
+           (set! ball_speed (list
+                             (car ball_speed)
+                             (round (- (car ball_speed) (/ (cadr ball_speed) (abs (cadr ball_speed))) )) ))])))
+
+; Cambia la direccion de la velocidad si el balon sale del campo
+; O lo coloca en el centro si es un gol
+(define (ball_out)
+  (cond [(< (car ball) MIN_X)
+         (begin (set! goals (list (car goals) (+ 1 (cadr goals))))
+                (set! ball (list CENTER_X CENTER_Y))
+                (set! ball_speed '(0 0)))]
+        [(> (car ball) MAX_X)
+         (begin (set! goals (list (+ 1 (car goals)) (cadr goals)))
+                (set! ball (list CENTER_X CENTER_Y))
+                (set! ball_speed '(0 0)))]
+        [(< (cadr ball) MIN_Y)
+         (begin
+           (set! ball (list (car ball) CENTER_Y))
+           (set! ball_speed (list (car ball_speed) (* -1 (cadr ball_speed))))
+           )]
+        [(> (cadr ball) MAX_Y)
+         (begin
+           (set! ball (list (car ball) CENTER_Y))
+           (set! ball_speed (list (car ball_speed) (* -1 (cadr ball_speed)))))]
+        ))
+
+
+; Comprueba si los jugadores terminaron su animacion
+(define (finished_anim)
+  (cond [(and
+          (equal? team_blue_keep selected_players_blue_keep)
+          (equal? team_blue_def selected_players_blue_def)
+          (equal? team_blue_mid selected_players_blue_mid)
+          (equal? team_blue_atck selected_players_blue_atck)
+          (equal? team_red_keep selected_players_red_keep)
+          (equal? team_red_def selected_players_red_def)
+          (equal? team_red_mid selected_players_red_mid)
+          (equal? team_red_atck selected_players_red_atck))
+         #t]
+        [else #f]))
+
 
 ; Loop principal de las animaciones
 (while #t
-       (send game_field on-paint)
-       (set! team1 (map update_pos team1 ball))
+       ; Aplica los algoritmos geneticos para crear nuevas poblaciones
 
-       (sleep/yield 0.03))
+       ; AZUL
+       ; Portero
+       (set! new_players (mutate team_blue_keep))
+       (set! selected_players_blue_keep (best_players team_blue_keep new_players ball LEFT_KEEPER))
+       ; Defensas
+       (set! new_players (mutate team_blue_def))
+       (set! selected_players_blue_def (best_players team_blue_def new_players ball LEFT_LIMIT))
+       ; Medios
+       (set! new_players (mutate team_blue_mid))
+       (set! selected_players_blue_mid (best_players team_blue_mid new_players ball CENTER_X))
+       ; Delanteros
+       (set! new_players (mutate team_blue_atck))
+       (set! selected_players_blue_atck (best_players team_blue_atck new_players ball RIGHT_LIMIT))
+
+       ; ROJO
+       ; Portero
+       (set! new_players (mutate team_red_keep))
+       (set! selected_players_red_keep (best_players team_red_keep new_players ball RIGHT_KEEPER))
+       ; Defensas
+       (set! new_players (mutate team_red_def))
+       (set! selected_players_red_def (best_players team_red_def new_players ball RIGHT_LIMIT))
+       ; Medios
+       (set! new_players (mutate team_red_mid))
+       (set! selected_players_red_mid (best_players team_red_mid new_players ball CENTER_X))
+       ; Delanteros
+       (set! new_players (mutate team_red_atck))
+       (set! selected_players_red_atck (best_players team_red_atck new_players ball LEFT_LIMIT))
+
+       (while (not (finished_anim))
+              (begin
+                ; Llama a la animacion de la cancha
+                (send game_field on-paint)
+
+                ; Consulta si alguien puede patear el balon y lo mueve
+                ; Azul
+                (for ([player team_blue_keep])
+                  (close_enough player 1))
+                (for ([player team_blue_def])
+                  (close_enough player 1))
+                (for ([player team_blue_mid])
+                  (close_enough player 1))
+                (for ([player team_blue_atck])
+                  (close_enough player 1))
+                ; Rojo
+                (for ([player team_red_keep])
+                  (close_enough player -1))
+                (for ([player team_red_def])
+                  (close_enough player -1))
+                (for ([player team_red_mid])
+                  (close_enough player -1))
+                (for ([player team_red_atck])
+                  (close_enough player -1))
+
+
+                ; Mueve los jugadores
+                ; Azul
+                (set! team_blue_keep (map (lambda (x y) (update_pos x y)) team_blue_keep selected_players_blue_keep))
+                (set! team_blue_def (map (lambda (x y) (update_pos x y)) team_blue_def selected_players_blue_def))
+                (set! team_blue_mid (map (lambda (x y) (update_pos x y)) team_blue_mid selected_players_blue_mid))
+                (set! team_blue_atck (map (lambda (x y) (update_pos x y)) team_blue_atck selected_players_blue_atck))
+                ; Rojo
+                (set! team_red_keep (map (lambda (x y) (update_pos x y)) team_red_keep selected_players_red_keep))
+                (set! team_red_def (map (lambda (x y) (update_pos x y)) team_red_def selected_players_red_def))
+                (set! team_red_mid (map (lambda (x y) (update_pos x y)) team_red_mid selected_players_red_mid))
+                (set! team_red_atck (map (lambda (x y) (update_pos x y)) team_red_atck selected_players_red_atck))
+
+                (sleep/yield 0.03)
+                ; Cambia la posicion del balon si fue pateado
+                (kicked)
+                (ball_out)))
+
+
+       (sleep/yield 0.03)
+       (cond [(or (= 3 (car goals)) (= 3 (cadr goals))) (break)])
+
+       )
 
 
--- genetics.rkt
+++ genetics.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; Comparte las funciones
+(provide (all-defined-out))
 
 ; FUNCIONES AUXILIARES
 
@@ -37,32 +39,44 @@
 ; Funcion auxiliar que crea los jugadores iniciales con estadisticas aleatorias
 (define (get_player_stats p stats_list)
   (cond [(= p 5) stats_list]
-        [(>= p 3) (get_player_stats (+ p 1) (cons (random 100) stats_list))]
-        [else (get_player_stats (+ p 1) (cons (random 5) stats_list))]))
+        [(>= p 3) (get_player_stats (+ p 1) (cons (+ 100 (random 500)) stats_list))]
+        [else (get_player_stats (+ p 1) (cons (random 1 3) stats_list))]))
 
 
+
+; Retorna los mejores jugadores de dos listas
+; Parametros: Antigua generacion, nueva generacion, posicion del balon, posicion deseada
+(define (best_players old_players new_players pos_ball best_pos)
+  (cond [(equal? '() (cdr old_players))
+         (cons (best (cons (car old_players) new_players) pos_ball best_pos) '())]
+        [else (cons (best (cons (car old_players) (cons (car new_players) '())) pos_ball best_pos)
+                    (best_players (cdr old_players) (cdr new_players) pos_ball best_pos) )]))
 
 ; Retorna el jugador con la mejor suma de estadisticas
-; Parametros: Lista de jugadores, posicion de la bola
-(define (best list_players pos_ball)
+; Parametros: Lista de jugadores, posicion de la bola, posicion deseada
+(define (best list_players pos_ball best_pos)
   (cond [(equal? '() (cdr list_players)) (car list_players)]
-        [else (cond [(is_better? (car list_players) (cadr list_players) pos_ball)
-                     (best [cons (car list_players) (cddr list_players) pos_ball])]
-                    [else (best [cdr list_players] pos_ball)])]))
+        [else (cond [(is_better? (car list_players) (cadr list_players) pos_ball best_pos)
+                     (best (cons (car list_players) (cddr list_players)) pos_ball best_pos)]
+                    [else (best [cdr list_players] pos_ball best_pos)])]))
 
 ; Compara dos jugadores y devuelve el mejor
-(define (is_better? first_p second_p pos_ball)
-  (cond [(> (suma first_p pos_ball) (suma second_p pos_ball)) #t]
+(define (is_better? first_p second_p pos_ball best_pos)
+  (cond [(> (suma first_p pos_ball best_pos) (suma second_p pos_ball best_pos)) #t]
         [else #f]))
 
 ; Devuelve la suma de los puntos de estadisticas de un jugador
-(define (suma player pos_ball)
+(define (suma player pos_ball best_pos)
   (+ [close_to_ball (car player) (cadr player) pos_ball] [caddr player] [cadddr player]
-     [cadddr (cdr player)]))
+     [cadddr (cdr player)] [close_to_best (car player) best_pos]))
 
 ; Retorna una calificacion de la distancia entre el jugador y el balon. Entre mas cerca mejor.
 (define (close_to_ball p_x p_y pos_ball)
-  (/ (- 100 (sqrt (+ (expt (- p_x (car pos_ball)) 2) (expt (- p_y (cadr pos_ball)) 2)))) 2))
+  (/ (- 1300 (sqrt (+ (expt (- p_x (car pos_ball)) 2) (expt (- p_y (cadr pos_ball)) 2)))) 15))
+
+; Retorna una calificacion de la distancia entre el jugador y el balon. Entre mas cerca mejor.
+(define (close_to_best p_x best_pos)
+  (/ (- 1300 (abs (- p_x best_pos))) 16))
 
 
 
@@ -99,17 +113,23 @@
   (cond [(equal? (cdr player) '())
          (cons (rnd_op (car player) 1 (random 3)) '())]
         [else (cond [(or (= n 0) (= n 1))
-                     (cons (rnd_op (car player) 5 (random 3)) (mutate_player (cdr player) (+ n 1)))]
+                     (cons (rnd_op (car player) 42 (random 3)) (mutate_player (cdr player) (+ n 1)))]
                     [else
                      (cons (rnd_op (car player) 1 (random 3)) (mutate_player (cdr player) (+ n 1)))])]))
 
 ; Opera de forma aleatoria (suma/resta/ninguno) un valor con un numero dado y retorna el resultado
 (define (rnd_op base value operator)
-  (cond [(= 0 operator) (+ base value)]
-        [(= 1 operator) (- base value)]
-        [else base]))
+  (cond [(= value 42)
+         (cond [(and (= 0 operator)) (+ base value)]
+               [(and (= 1 operator)) (- base value)]
+               [else base])]
+        [else
+         (cond [(and (= 0 operator) (< base 10)) (+ base value)]
+               [(and (= 1 operator) (> base 1)) (- base value)]
+               [else base])]))
 
 
 ;(new_child (reverse '(23 24 1 2 3)) (reverse'(32 32 4 5 6)) 4 '())
 ;(reproduce '((20 22 1 2 3) (30 33 4 5 6)) 5 '())
 ; (mutate '((22 22 3 4 5)))
+; (best_players '((33 33 5 5 5) (20 20 4 6 9)) '((40 40 6 2 8) (60 60 4 6 1)) '(50 50))
